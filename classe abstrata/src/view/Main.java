41c01b591f4d95285b99fbcc3354b2c2cf690f82
Alex_Souza2002 <62069087+AlexdeSouza2502@users.noreply.github.com>
2023-04-11 23:00:48 -0300

    Add files via upload

---
+++ classe abstrata/src/view/Main.java
@@ -0,0 +1,25 @@
+package view;
+
+import Model.AtualizadorDeContas;
+import Model.ContaCorrente;
+import Model.ContaPoupanca;
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+    
+    ContaCorrente cc = new ContaCorrente();
+    ContaPoupanca cp = new ContaPoupanca();
+
+    
+    cc.deposita(1000);
+    cp.deposita(1000);
+
+    AtualizadorDeContas adc = new AtualizadorDeContas(0.10);
+    
+    adc.roda(cc);
+    adc.roda(cp);
+    System.out.println("\nSaldo Total: " + adc.getSaldoTotal());
+  }
+}
